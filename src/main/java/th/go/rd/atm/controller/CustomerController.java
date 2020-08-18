e3f8ebc8c40ca7693796f16a6d32878cf862d3d1
ladyusa <usa.sa@ku.th>
2020-08-19 00:51:25 +0700

    get customer objects

--- src/main/java/th/go/rd/atm/controller/CustomerController.java
+++ src/main/java/th/go/rd/atm/controller/CustomerController.java
@@ -4,12 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class CustomerController {
 
     @RequestMapping("/customer")
     public String getCustomerPage(Model model) {
-        model.addAttribute("allCustomers", new String[]{"Peter", "Nancy", "Rick"});
+        List<Customer> customerList= new ArrayList<>();
+        customerList.add(new Customer(1,"Peter",1234));
+        customerList.add(new Customer(2,"Nancy",2345));
+        customerList.add(new Customer(3,"Rick",3456));
+        model.addAttribute("allCustomers", customerList);
         return "customer";
     }
 }
