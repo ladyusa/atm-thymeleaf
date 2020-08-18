e7607aa1448b098e8fd7509b99128ab3c8e24cd8
ladyusa <usa.sa@ku.th>
2020-08-19 01:11:44 +0700

    post customer

--- src/main/java/th/go/rd/atm/controller/CustomerController.java
+++ src/main/java/th/go/rd/atm/controller/CustomerController.java
@@ -2,21 +2,31 @@ package th.go.rd.atm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import th.go.rd.atm.model.Customer;
+import th.go.rd.atm.service.CustomerService;
 
 @Controller
 public class CustomerController {
 
-    @RequestMapping("/customer")
+    private CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/customer")
     public String getCustomerPage(Model model) {
-        List<Customer> customerList= new ArrayList<>();
-        customerList.add(new Customer(1,"Peter",1234));
-        customerList.add(new Customer(2,"Nancy",2345));
-        customerList.add(new Customer(3,"Rick",3456));
-        model.addAttribute("allCustomers", customerList);
+        model.addAttribute("allCustomers", customerService.getCustomers());
         return "customer";
+    }
+
+    @PostMapping("/customer")
+    public String registerCustomer(@ModelAttribute Customer customer, Model model) {
+        customerService.createCustomer(customer);
+        model.addAttribute("allCustomers", customerService.getCustomers());
+        return "redirect:customer";
     }
 }
